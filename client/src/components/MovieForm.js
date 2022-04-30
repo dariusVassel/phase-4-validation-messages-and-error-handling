e08a0793f9bcbf3e72dedb79615d1ec0bd8598ae
+ <+>
2022-04-30 12:58:36 -0400

    Complete

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -14,18 +14,27 @@ function MovieForm() {
     female_director: false,
   });
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/movies", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+  const [errors, setErrors] = useState([])
+
+  // make the function async to enable the await keyword
+async function handleSubmit(e) {
+  e.preventDefault();
+  // fetch returns a Promise, we must await it
+  const response = await fetch("/movies", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+  },
+  body: JSON.stringify(formData),
+  });
+  // response.json() returns a Promise, we must await it
+  const data = await response.json();
+    if (response.ok) {
+      console.log("Movie created:", data);
+    } else {
+      setErrors(data.errors);
   }
+}
 
   function handleChange(e) {
     const value =
@@ -125,7 +134,14 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
-        <SubmitButton type="submit">Add Movie</SubmitButton>
+        {errors.length > 0 && (
+        <ul style={{ color: "red" }}>
+          {errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+        )}
+        <SubmitButton type="submit">Add Movie</SubmitButton> 
       </form>
     </Wrapper>
   );
